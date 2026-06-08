ca0edb81edc74bcc640eeb4ec4567f6cdce15fb5
Toby Cm <newtobycm@gmail.com>
2026-06-08 09:49:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let hidden = false;
+
+card = document.getElementsByClassName("card").item(0)
+
+console.log(card)
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('powerBtn');
+    if (!btn) return;
+  
+    btn.addEventListener('click', () => {
+      hidden = !hidden;
+
+      card.style.opacity = hidden ? 0.1 : 1;
+
+      btn.textContent = hidden ? 'Show' : "Hide"
+    });
+  });
+  
